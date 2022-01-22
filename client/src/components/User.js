dd211d863fd115915e408bb71d659ae2d2305b56
rgoshen <rick.goshen@gmail.com>
2022-01-21 19:50:18 -0700

    add edit user

--- client/src/components/User.js
+++ client/src/components/User.js
@@ -1,15 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Form from 'react-bootstrap/Form';
 
-function User({ id, user }) {
+function User({ id, user, handleEdit, handleDelete }) {
+  const [formData, setFormData] = useState({
+    firstName: user.firstName,
+    lastName: user.lastName,
+    email: user.email,
+    state: user.state,
+  });
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
   return (
     <>
       <td>{user.id}</td>
-      <td>{user.firstName}</td>
-      <td>{user.lastName}</td>
-      <td>{user.email}</td>
-      <td>{user.state}</td>
-      <td>{}</td>
-      <td>{}</td>
+      <td>
+        <Form.Control
+          type='text'
+          name='firstName'
+          value={formData.firstName}
+          onChange={onChange}
+        />
+      </td>
+      <td>
+        <Form.Control
+          type='text'
+          name='lastName'
+          value={formData.lastName}
+          onChange={onChange}
+        />
+      </td>
+      <td>
+        <Form.Control
+          type='email'
+          name='email'
+          value={formData.email}
+          onChange={onChange}
+        />
+      </td>
+      <td>
+        <Form.Select name='state' value={formData.state} onChange={onChange}>
+          <option value='active' name='active'>
+            Active
+          </option>
+          <option value='pending' name='pending'>
+            Pending
+          </option>
+        </Form.Select>
+      </td>
+      <td>
+        <i
+          className='fas fa-edit'
+          onClick={() => handleEdit(formData, user.id)}
+        ></i>
+      </td>
+      <td>
+        <i
+          className='fas fa-trash-alt'
+          onClick={() => handleDelete(user.id)}
+        ></i>
+      </td>
     </>
   );
 }
--- client/src/components/Users.js
+++ client/src/components/Users.js
@@ -3,9 +3,13 @@ import axios from 'axios';
 import { baseURL } from '../config';
 import Table from 'react-bootstrap/Table';
 import User from './User';
+import Alert from 'react-bootstrap/Alert';
 
 function Users() {
   const [users, setUsers] = useState([]);
+  const [deleted, setDeleted] = useState(false);
+  const [edited, setEdited] = useState(false);
+
   const getAllUsers = () => {
     axios
       .get(baseURL)
@@ -16,15 +20,66 @@ function Users() {
       .catch((err) => console.error(err));
   };
 
+  const handleDelete = (id) => {
+    axios
+      .delete(`${baseURL}/${id}`)
+      .then((res) => {
+        const user = res.data;
+        console.log(user);
+        setUsers(users.filter((user) => user.id !== id));
+        setDeleted(true);
+      })
+      .catch((err) => console.error(err));
+  };
+
+  const handleEdit = (formData, id) => {
+    const userData = { ...formData, id };
+    axios
+      .put(`${baseURL}/${id}`, userData)
+      .then((res) => {
+        setUsers((users) =>
+          users.map((user) => (user.id === id ? res.data : user))
+        );
+        setEdited(true);
+      })
+      .catch((err) => console.log(err));
+  };
+
   useEffect(() => {
     getAllUsers();
     console.log(users);
   }, []);
 
+  useEffect(() => {
+    if (deleted) {
+      let resetDeletedTimer = setTimeout(() => {
+        setDeleted(false);
+      }, 2000);
+
+      return () => {
+        clearTimeout(resetDeletedTimer);
+      };
+    }
+
+    if (edited) {
+      let resetEditedTimer = setTimeout(() => {
+        setEdited(false);
+      }, 2000);
+
+      return () => {
+        clearTimeout(resetEditedTimer);
+      };
+    }
+  }, [deleted, edited]);
+
   return (
     <div className='constainer'>
       <h1 className='m-5'>Yodlr Admin Page</h1>
-      <div className='container'>
+      <div className='container alert'>
+        {deleted && <Alert variant='success'>User has been deleted!</Alert>}
+        {edited && <Alert variant='success'>User has been updated!</Alert>}
+      </div>
+      <div className='container shadow-lg'>
         <Table striped bordered hover>
           <thead>
             <tr>
@@ -38,10 +93,15 @@ function Users() {
             </tr>
           </thead>
           <tbody>
-            {users.map((user) => {
+            {users.map((user, index) => {
               return (
                 <tr key={user.id}>
-                  <User id={user.id} user={user} />
+                  <User
+                    id={index}
+                    user={user}
+                    handleEdit={handleEdit}
+                    handleDelete={handleDelete}
+                  />
                 </tr>
               );
             })}
